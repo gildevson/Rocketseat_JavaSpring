04fb5bdf0e9eb26be9e51a927325a6973d1daea4
Gilson Fonseca <gildevson@gmail.com>
2025-08-05 20:15:57 -0300

    Adicionando task de Tabela

---
+++ src/main/java/br/com/rocketseat/todolist/task/TaskModel.java
@@ -0,0 +1,41 @@
+package br.com.rocketseat.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+    /*
+    * ID
+    * Usuario (ID_USUARIO)
+    * Descrição
+    * Titulo
+    * Data de Inicio
+    * Data de Terminio
+    * Prioridade
+    * */
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String descripton;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt; // data e horario
+    private LocalDateTime endAt; // Quando ele vai finalizar a tarefa
+    private String priority; // prioridade
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    private UUID idUser;
+}
--- src/main/java/br/com/rocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -43,7 +43,7 @@ public class UserController {
 
 
         var userCreated = this.userRepository.save(userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);                                      // é obrigatorio adicionar ResponseEntity
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);// é obrigatorio adicionar ResponseEntity
 
     }
 }
